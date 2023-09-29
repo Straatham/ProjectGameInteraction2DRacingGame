76899afefb852de189e8fadf257d89f580691f54
Straatham <70264213+Straatham@users.noreply.github.com>
2023-09-29 15:39:02 +0200

    Main Menu Figma in WPF
    + Gitignore shit proberen te fixen

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace ProjectGameInteraction2DRacingGame
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
     }
 }
