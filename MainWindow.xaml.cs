eb048cc6b4a90971c7b5780cd3bc8b502f57d1c3
Straatham <70264213+Straatham@users.noreply.github.com>
2023-10-05 16:44:00 +0200

    Testing week 4

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,9 +10,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ProjectGameInteraction2DRacingGame
 {
@@ -24,10 +27,66 @@ namespace ProjectGameInteraction2DRacingGame
         {
             InitializeComponent();
         }
+        private bool _allowDirectNavigation = false;
+        private NavigatingCancelEventArgs _navArgs = null;
+        private Duration _duration = new Duration(TimeSpan.FromMilliseconds(100));
+        private double _oldOpacity = 0;
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Main method to call if you want to fade in/out frames
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Frame_Navigating(object sender, NavigatingCancelEventArgs e)
         {
-            Environment.Exit(0);
+            if (Content != null && !_allowDirectNavigation)
+            {
+                e.Cancel = true;
+
+                _navArgs = e;
+                _oldOpacity = MainFrameWindow.Opacity;
+
+                DoubleAnimation animation0 = new DoubleAnimation();
+                animation0.From = MainFrameWindow.Opacity;
+                animation0.To = 0;
+                animation0.Duration = _duration;
+                animation0.Completed += SlideCompleted;
+                MainFrameWindow.BeginAnimation(OpacityProperty, animation0);
+            }
+            _allowDirectNavigation = false;
+        }
+
+        private void SlideCompleted(object sender, EventArgs e)
+        {
+            _allowDirectNavigation = true;
+            switch (_navArgs.NavigationMode)
+            {
+                case NavigationMode.New:
+                    if (_navArgs.Uri == null)
+                        MainFrameWindow.Navigate(_navArgs.Content);
+                    else
+                        MainFrameWindow.Navigate(_navArgs.Uri);
+                    break;
+                case NavigationMode.Back:
+                    MainFrameWindow.GoBack();
+                    break;
+                case NavigationMode.Forward:
+                    MainFrameWindow.GoForward();
+                    break;
+                case NavigationMode.Refresh:
+                    MainFrameWindow.Refresh();
+                    break;
+            }
+
+            Dispatcher.BeginInvoke(DispatcherPriority.Loaded,
+                (ThreadStart)delegate ()
+                {
+                    DoubleAnimation animation0 = new DoubleAnimation();
+                    animation0.From = 0;
+                    animation0.To = _oldOpacity;
+                    animation0.Duration = _duration;
+                    MainFrameWindow.BeginAnimation(OpacityProperty, animation0);
+                });
         }
     }
 }
---
+++ Pages/InstellingenPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectGameInteraction2DRacingGame.Pages
+{
+    /// <summary>
+    /// Interaction logic for InstellingenPage.xaml
+    /// </summary>
+    public partial class InstellingenPage : Page
+    {
+        MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>()?.FirstOrDefault();
+        public InstellingenPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Terug_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MainMenuPage setPage = new MainMenuPage();
+                NavigationService.Navigate(setPage);
+            }
+            catch
+            {
+                MessageBox.Show($"{mainWindow} is null (not found). Exiting....");
+                Environment.Exit(0);
+            }
+        }
+    }
+}
