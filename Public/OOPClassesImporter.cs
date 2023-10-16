5ed29969621fdf55e709400d0c8a957ee23ae5e0
ThomasOlifant <144350455+ThomasOlifant@users.noreply.github.com>
2023-10-16 11:55:39 +0200

    Update OOPClassesImporter.cs

--- Public/OOPClassesImporter.cs
+++ Public/OOPClassesImporter.cs
@@ -5,18 +5,21 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace ProjectGameInteraction2DRacingGame.Public
 {
+    
     public class OOPClassesImporter
     {
         public static List<CarClass> ImportClasses()
         {
+            var path = Path.Combine("/OOPTextfiles", "CarClasses.txt");
             List<CarClass> list = new List<CarClass>();
 
-            if (File.Exists(@AppDomain.CurrentDomain.BaseDirectory + @"\GameData\" + "CarClasses.txt"))
+            if (File.Exists(path))
             {
-                foreach (string file in File.ReadAllLines(AppDomain.CurrentDomain.BaseDirectory + @"\GameData\" + "CarClasses.txt").Skip(1))
+                foreach (string file in File.ReadAllLines(path).Skip(1))
                 {
                     string[] split2 = file.Split(',');
                     list.Add(new CarClass(int.Parse(split2[0]), split2[1]));
@@ -28,11 +31,12 @@ namespace ProjectGameInteraction2DRacingGame.Public
         }
         public static List<Track> ImportTracks()
         {
+            var path = Path.Combine("/OOPTextfiles", "Tracks.txt");
             List<Track> list = new List<Track>();
 
-            if (File.Exists(@AppDomain.CurrentDomain.BaseDirectory + @"\GameData\" + "Tracks.txt"))
+            if (File.Exists(path))
             {
-                foreach (string file in File.ReadAllLines(AppDomain.CurrentDomain.BaseDirectory + @"\GameData\" + "Tracks.txt").Skip(1))
+                foreach (string file in File.ReadAllLines(path).Skip(1))
                 {
                     string[] split2 = file.Split(',');
                     list.Add(new Track(int.Parse(split2[0]), int.Parse(split2[1]), int.Parse(split2[2]), split2[3], split2[4], TimeSpan.Parse(split2[5])));
