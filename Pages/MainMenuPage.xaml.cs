3b554c8a721a7df30c0c900a5da7e15ece0c47c7
3DSBricker <66943676+3DSBricker@users.noreply.github.com>
2023-11-02 09:23:40 +0100

    Revert "Matthijs - Translation"

--- Pages/MainMenuPage.xaml.cs
+++ Pages/MainMenuPage.xaml.cs
@@ -1,6 +1,4 @@
-﻿using Newtonsoft.Json;
-using ProjectGameInteraction2DRacingGame.Components;
-using ProjectGameInteraction2DRacingGame.OOP;
+﻿using ProjectGameInteraction2DRacingGame.OOP;
 using ProjectGameInteraction2DRacingGame.Public;
 using System;
 using System.Collections.Generic;
@@ -18,8 +16,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.IO;
-using static ProjectGameInteraction2DRacingGame.Components.LanguageManager;
 
 namespace ProjectGameInteraction2DRacingGame.Pages
 {
@@ -33,37 +29,6 @@ namespace ProjectGameInteraction2DRacingGame.Pages
         {
             InitializeComponent();
             mainWindow.player.Play();
-            Loaded += NavigationService_Navigated;
-        }
-        private void NavigationService_Navigated(object sender, RoutedEventArgs e)
-        {
-            OnLanguageSwitchRequested();
-        }
-        public string LoadSelectedLanguage()
-        {
-            if (File.Exists("cache.json"))
-            {
-                // Lees de opgeslagen JSON uit het cachebestand
-                string json = File.ReadAllText("cache.json");
-
-                // Deserialiseer het JSON naar een object
-                var languageData = JsonConvert.DeserializeObject<LanguageData>(json);
-
-                return languageData.LanguageCode;
-            }
-
-            return "nl"; // Stel een standaard taalcode in als er niets is opgeslagen
-        }
-        public void OnLanguageSwitchRequested()
-        {
-            string languageCode = LoadSelectedLanguage();
-            ResourceDictionary dict = new()
-            {
-                Source = new Uri($"../Resources/Strings.{languageCode}.xaml", UriKind.Relative)
-            };
-
-            Resources.MergedDictionaries.Clear();
-            Resources.MergedDictionaries.Add(dict);
         }
         private void ButtonSpeel_Click(object sender, RoutedEventArgs e)
         {
